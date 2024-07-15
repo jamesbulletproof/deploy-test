ed4615c4b78a68590cb96b953b2296630e6c21f1
jamesbulletproof <113986445+jamesbulletproof@users.noreply.github.com>
2024-07-15 14:44:13 +0100

    made a massive change

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,13 +10,7 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
+      <h1>Massive change to it here</h1>
     </Layout>
   );
 }
